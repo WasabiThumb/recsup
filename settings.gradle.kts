d3d8a57758549df1370a86cba7d181d635897661
WasabiThumb <xpedraza542@gmail.com>
2026-09-23 22:45:23 -0400

    Use foojay-resolver-convention

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,11 @@
 rootProject.name = "recsup"
+
+pluginManagement {
+    plugins {
+        id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+    }
+}
+
 include(
     ":facets:base",
     ":facets:never",
