e5b843f0b9a6e553b2cb2036593ec1ac8960dd68
WasabiThumb <xpedraza542@gmail.com>
2026-09-23 22:48:27 -0400

    Disable configuration cache

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,4 @@
 rootProject.name = "recsup"
-
-pluginManagement {
-    plugins {
-        id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-    }
-}
-
 include(
     ":facets:base",
     ":facets:never",
